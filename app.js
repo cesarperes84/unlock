60e1b4003766802efacc1e51447b46ea9abd4993
Cesar Peres <cperes84@gmail.com>
2023-11-15 01:33:17 -0300

    chore: adicionar imagens

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ let speedBallY = 10;
 let clickStartGame = false;
 var angleBall = 0;
 let hit = false;
-const rightAngle = 90;
-var url1 = 'assets/img/bullet.png';
-var url2 = 'assets/img/trajectory.png';
+var rightAngle = 220;
+const url1 = 'assets/img/bullet.png';
+const url2 = 'assets/img/trajectory.png';
+let pause = false;
 var imageBall = new Image();
 var imagePath = new Image();
 
@@ -24,8 +25,7 @@ const setup = () => {
 
 const startGame = () => {
     if (!clickStartGame) {
-        setInterval(loop, 50000/60);
-        handleBallOrientation();
+        setInterval(loop, 10);
         loadImages();
     }
     clickStartGame = true;
@@ -35,43 +35,51 @@ const loop = () => {
     draw();
 };
 
-const handleBallOrientation = () => {
-    ballOrientation *= -1;
-};
-
-const draw = () => {
-    angleBall += 1;
-    // console.log('angleBall1', Math.ceil(angleBall));
-    // drawPath(10, 20, 300, 300);
-    drawBall(20, 20);
+const goNextLevel = () => {
+    // ballOrientation *= -1;
+    setTimeout(() => {
+        rightAngle = Math.floor(Math.random() * 360);
+        hit = false;
+        angleBall = 0;
+        pause = false;
+    }, 3000)
     
 };
 
-const drawBall = (x, y)  => {
-    if (!hit) {
-        
-        ctx.clearRect(0, 0, w, h);
-        ctx.imageSmoothingEnabled = true;
-        // Path
-        scale = 0.9;
-        ctx.save();
-        ctx.translate(x + imageBall.width * scale / 2, y + imageBall.height * scale / 2);
-        ctx.rotate(rightAngle);
-        ctx.translate(- x - imageBall.width * scale / 2, - y - imageBall.height * scale / 2);
-        ctx.drawImage(imagePath, x, y, imageBall.width * scale, imageBall.height * scale);
-        ctx.restore();
-
-        // Ball
-        ctx.save();
-        ctx.translate(x + imageBall.width * scale / 2, y + imageBall.height * scale / 2);
-        console.log('angleB', angleBall)
-        ctx.rotate(angleBall);
-        // ctx.rotate(280);
-        ctx.translate(- x - imageBall.width * scale / 2, - y - imageBall.height * scale / 2);
-        ctx.drawImage(imageBall, x, y, imageBall.width * scale, imageBall.height * scale);
-        ctx.restore();
-        writeScore();
+const draw = () => {
+    if (!hit && !pause) {
+        if (angleBall === 360) angleBall = 0;
+        angleBall += 1;
+        angleBall = parseFloat(angleBall.toFixed(2));
+        drawBall(20, 20);
+    } else {
+        angleBall = rightAngle;
+        drawBall(20, 20);
+        pause = true;
     }
+};
+
+const drawBall = (x, y)  => {
+    ctx.clearRect(0, 0, w, h);
+    ctx.imageSmoothingEnabled = true;
+    scale = 0.9;
+
+    // Path
+    ctx.save();
+    ctx.translate(x + imagePath.width * scale / 2, y + imagePath.height * scale / 2);
+    ctx.rotate(rightAngle * Math.PI / 180);
+    ctx.translate(- x - imagePath.width * scale / 2, - y - imagePath.height * scale / 2);
+    ctx.drawImage(imagePath, x, y, imageBall.width * scale, imagePath.height * scale);
+    ctx.restore();
+    
+    // Ball
+    ctx.save();
+    ctx.translate(x + imageBall.width * scale / 2, y + imageBall.height * scale / 2);
+    ctx.rotate(angleBall * Math.PI / 180);
+    ctx.translate(- x - imageBall.width * scale / 2, - y - imageBall.height * scale / 2);
+    ctx.drawImage(imageBall, x, y, imageBall.width * scale, imageBall.height * scale);
+    ctx.restore();
+    writeScore();
 };
 
 const writeScore = () => {
@@ -81,12 +89,11 @@ const writeScore = () => {
 };
 
 const hitCheck = (ev) => {
-    
-    if (clickStartGame) {       
-        if (angleBall === rightAngle) {
+    if (clickStartGame && !pause) {  
+       if (angleBall > (rightAngle-5) && angleBall <= (rightAngle+5)) {  
             points++;
             hit = true;
-            handleBallOrientation();
+            goNextLevel();
         }
     }
 }
